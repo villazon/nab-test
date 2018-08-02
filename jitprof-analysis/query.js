ae66ffa2cfa649a6782af3dc68883a01d43382c8
Haiyang Sun <sunh@usi.ch>
2018-08-02 12:31:53 +0200

    updated query for result

--- jitprof-analysis/query.js
+++ jitprof-analysis/query.js
@@ -4,21 +4,23 @@ const assert = require('assert');
 // Connection URL
 const url = 'mongodb://localhost:27017';
  
-// Database Name
-const dbName = 'NODEPROFDB';
- 
 // Use connect method to connect to the server
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
  
-  const db = client.db(dbName);
+  const db = client.db("NODEPROFDB");
 
   const collections = db.collection("jitprof");
+  const db_ds = client.db("dsProject");
+  const projects = db_ds.collection("results");
 
-  collections.find({}).forEach(function(doc){
-    console.log(doc);
-  });
- 
-  client.close();
+  collections.find({}).snapshot().forEach(function(doc){
+    projects.find({hash: doc.hash}).forEach(function(prj) {
+      console.log(doc.reponame+" == "+prj.repo);
+    }
+  )}).then(
+    ()=> {client.close()}
+  );
+
 });
