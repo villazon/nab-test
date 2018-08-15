4296325c1f4ee8ab0db9d9343e56a407339118be
villazon <alex.villazon@gmail.com>
2018-08-15 19:41:02 -0400

    wait

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // do not require
+
+function wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+	end = new Date().getTime();
+    }
+}
+
+
 function testNAB() {
     var arr = [1,2];
     arr[6] = 1; // creates non-contiguous array;
@@ -6,4 +16,5 @@ function testNAB() {
     console.log("arg non-contigous ARRAY " + arr[6]);
 }
 
-testNAB();
+wait(10000);
+
